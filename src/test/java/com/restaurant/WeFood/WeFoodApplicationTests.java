deb126998e3e4b9a8e93f9a103972307cdc7b4ed
Alecio <aleciosilveira97@gmail.com>
2025-08-30 00:37:46 -0300

    Projeto Inicial

---
+++ src/test/java/com/restaurant/WeFood/WeFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.WeFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
